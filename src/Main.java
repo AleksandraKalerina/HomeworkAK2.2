841f43c9dde36c6137ee75dbf4a3b6588585783b
Aleks Ka <aleksandra.bm@mail.ru>
2024-02-21 21:10:47 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        int balans = 85;
+        int payment = 1500;
+        int bonus;
+
+        if (payment > 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int chek = balans + payment + bonus;
+
+
+        System.out.println("Итоговый баланс: " + chek + " Итого бонус: " + bonus);
+    }
 }
